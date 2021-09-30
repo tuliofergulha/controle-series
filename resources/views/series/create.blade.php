60e833ef21c92b54be2ad7cef057ee30f04d02ee
Túlio Fergulha <tfergulha@gmail.com>
2021-09-29 22:26:42 -0300

    Refatoração das views e criada base de dados

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+@section('cabecalho')
+    Adicionar Série
+@endsection('cabecalho)
+
+@section('conteudo')
+    <form method="post">
+        <div class="form-group">
+            <label for="nome" class="">Nome</label>
+            <input type="text" class="form-control" name="nome" id="nome">
+        </div>
+
+        <button class="btn btn-success mt-2">Salvar</button>
+    </form>
+@endsection('conteudo')
---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+@section('cabecalho')
+    Séries
+@endsection('cabecalho')
+
+@section('conteudo')
+    <a href="/series/criar" class="btn btn-success mb-2">Adicionar</a>
+
+    <ul class="list-group">
+        @foreach ($series as $serie)
+        <li class="list-group-item"><?= $serie; ?></li>
+        @endforeach
+    </ul>
+@endsection('conteudo')
